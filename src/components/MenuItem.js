c9ec1ce528dc9c24b1c4fe84922949f8c7f8ce55
Adam Olson <40580569+Hot-Lobster@users.noreply.github.com>
2023-05-31 18:31:22 -0500

    added more functionality to the cart

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,45 +1,112 @@
+// import React, { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+ 
+// const Menu = ({menu}) => (
+//   <tr>
+//     <td>{menu.name}</td>
+//     <td>{menu.price}</td>
+//     <td>{menu.ingredients}</td>
+//   </tr>
+//  );
+
+// export default function MenuList() {
+//  const [items, setMenu] = useState([]);
+ 
+//  // This method fetches the items from the database.
+//  useEffect(() => {
+//    async function getMenu() {
+//      const response = await fetch(`http://localhost:5050/menu/`);
+ 
+//      if (!response.ok) {
+//        const message = `An error occurred: ${response.statusText}`;
+//        window.alert(message);
+//        return;
+//      }
+ 
+//      const items = await response.json();
+//      console.log(items);
+//      setMenu(items);
+//    }
+ 
+//   //  getMenu();
+//    getMenu().catch(error => console.error(error));
+
+//    return;
+//  }, [items.length]);
+
+// function menuList() {
+//   return items.map(({name, price, ingredients}, index) => {
+//     return (
+//       <Menu
+//         key={index}
+//         menu={{name, price, ingredients}}
+//       />
+//     );
+//   });
+// }
+
+//  return (
+//   <div>
+//     <h3>Menu</h3>
+//     <table className="table table-striped" style={{ marginTop: 20 }}>
+//     <thead>
+//       <tr>
+//         <th>Name</th>
+//         <th>Price</th>
+//         <th>Ingredients</th>
+//       </tr>
+//     </thead>
+//       <tbody>{menuList()}</tbody>
+//     </table>
+//   </div>
+// );
+// }
+ 
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
- 
-const Menu = ({menu}) => (
-  <tr>
-    <td>{menu.name}</td>
-    <td>{menu.price}</td>
-    <td>{menu.ingredients}</td>
-  </tr>
- );
+import "./menuItem.css";
 
+const MenuCard = ({menu, handleAddToCart}) => (
+  <div className="card" style={{width: '18rem', margin: '10px'}}>
+    <div className="card-body">
+      <h5 className="card-title">{menu.name}</h5>
+      <h6 className="card-subtitle mb-2 text-muted" >{menu.price}</h6>
+      <h7 className="card-subtitle mb-2 ">{menu.ingredients}</h7>
+      <div className="mt-3">
+        <button className="btn btn-primary" onClick={alert}>Add to Cart</button>
+      </div>
+    </div>
+  </div>
+);
 export default function MenuList() {
  const [items, setMenu] = useState([]);
- 
- // This method fetches the items from the database.
+
  useEffect(() => {
    async function getMenu() {
      const response = await fetch(`http://localhost:5050/menu/`);
- 
+
      if (!response.ok) {
        const message = `An error occurred: ${response.statusText}`;
        window.alert(message);
        return;
      }
- 
+
      const items = await response.json();
      console.log(items);
      setMenu(items);
    }
- 
-  //  getMenu();
+
    getMenu().catch(error => console.error(error));
 
    return;
  }, [items.length]);
 
 function menuList() {
-  return items.map(({name, price, ingredients}, index) => {
+  return items.map((menu, index) => {
     return (
-      <Menu
+      <MenuCard
         key={index}
-        menu={{name, price, ingredients}}
+        menu={menu}
       />
     );
   });
@@ -48,17 +115,9 @@ function menuList() {
  return (
   <div>
     <h3>Menu</h3>
-    <table className="table table-striped" style={{ marginTop: 20 }}>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Price</th>
-        <th>Ingredients</th>
-      </tr>
-    </thead>
-      <tbody>{menuList()}</tbody>
-    </table>
+    <div className="d-flex flex-wrap justify-content-start" style={{ marginTop: 20 }}>
+      {menuList()}
+    </div>
   </div>
 );
 }
- 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -22,7 +22,7 @@ function NavigationBar() {
   }, []);
 
   return (
-    <nav className="navbar">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary ">
       <Link to="/" className="navbar-logo-link">
         <img src={logo} alt="Logo" className="navbar-logo" />
       </Link>
@@ -78,3 +78,61 @@ function NavigationBar() {
 }
 
 export default NavigationBar;
+
+// return (
+//   <nav className="navbar">
+//     <Link to="/" className="navbar-logo-link">
+//       <img src={logo} alt="Logo" className="navbar-logo" />
+//     </Link>
+//     <ul className="navbar-nav">
+//       <li className="nav-item">
+//         <Link to="/" className="nav-link">
+//           Home
+//         </Link>
+//       </li>
+//       <li className="nav-item">
+//         <Link to="/menu" className="nav-link">
+//           Menu
+//         </Link>
+//       </li>
+//       {!loggedIn && (
+//       <>
+//         <li className="nav-item">
+//           <Link to="/sign-in" className="nav-link">
+//             Sign In
+//           </Link>
+//         </li>
+//         <li className="nav-item">
+//           <Link to="/order-online" className="nav-link">
+//             Order Online
+//           </Link>
+//         </li>
+//         <li className="nav-item">
+//           <Link to="/locations" className="nav-link">
+//           Locations
+//           </Link>
+//       </li>
+//         <li className="nav-item">
+//         <Link to="/our-story" className="nav-link">
+//           Our Story
+//         </Link>
+//       </li>
+//       <li className="nav-item">
+//         <Link to="/contact-us" className="nav-link">
+//           Contact Us
+//         </Link>
+//       </li>
+//       </>
+//     )}
+//         <li>
+//         <Link to="/checkout" className="nav-link cart-link" style={{ marginLeft: "auto" }}>
+//           <div className="cart-count">{totalItems}</div>
+//             <FontAwesomeIcon icon={faShoppingCart} />
+//         </Link>
+//       </li>
+//     </ul>
+//   </nav>
+// );
+// }
+
+// export default NavigationBar;
