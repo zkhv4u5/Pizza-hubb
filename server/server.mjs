9983a0cfe369dbcc35b0141acdc67ce1c27f3498
Adam Olson <40580569+Hot-Lobster@users.noreply.github.com>
2023-06-01 20:04:27 -0500

    update

--- server/server.mjs
+++ server/server.mjs
@@ -19,9 +19,9 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   //*Set static folder up in production
-  app.use(express.static('client/build'));
+  app.use(express.static('src/build'));
 
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'src', 'build','index.html')));
 }
 
 app.use("/",router)
